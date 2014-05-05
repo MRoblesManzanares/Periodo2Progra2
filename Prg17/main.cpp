8df94c94256458c1e1e476ec4d967e2187639cbf
MRoblesManzanares <Marvin_robles@unitec.edu>
2014-05-05 17:49:21 +0200

    Actualizacion 6

---
+++ Prg17/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+/* Ingresar un numero divisible entre,validarlo luego preguntar
+si desea continuar, alfinal presentar la suma de los numeros */
+int main()
+{
+    do{
+
+    }while(resp!=N)
+    //presentar la suma de los numeros divisible a 5.
+}
